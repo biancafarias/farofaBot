7945b4967407177ecabbe2b4e42f3772baee5bf0
biancafarias <biiancazinha@live.com>
2015-04-18 03:55:48 -0300

    extension.js

--- extension.js
+++ extension.js
@@ -16,23 +16,18 @@
         bot.retrieveSettings();
 
         bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
-                    'deu-lhe um biscoito de aveia caseiro macio!',
-                    'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
-                    'deu-lhe um bolinho de açúcar... O quê? Sem estrelinhas e povilho? Eu não tocaria.',
-                    'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
-                    'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... estranho',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Por que você não está trabalhando?"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Cumprimente agora a pessoa que você ama"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Arrisque-se!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Não esqueça de comer os vegetais"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão te achar sexy!',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo"',
-                    'deu-lhe um biscoito de ouro, mas não dá pra comer... Droga!',
-                    'deu-lhe um Oreo e um copo de leite.',
+                    'Quer te fuder até o talo!( ͡° ͜ʖ ͡°)',
+                    'deu-lhe um biscoito que está escrito "você é um viado"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Vou te dar um ban eterno de 30 dias"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Mostra as teta ae :trollface:"',
+                    'deu-lhe um cookie vencido!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Você é um fracassado!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão achar que você é gay!',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo *-*"',
+                    'deu-lhe um Trakinas e um copo de refrigerante',
                     'deu-lhe um biscoito de arco-íris feito com amor :heart:',
-                    'deu-lhe um biscoito que fio esquecido na chuva... eu não comeria.',
-                    'te trouxe biscoitos fresquinhos... parecem deliciosos!'
+                    'deu-lhe um biscoito que foi esquecido na chuva... eu não comeria.',
+                    'te trouxe biscoitos fresquinhos... parecem deliciosos! :3'
                 ];
                 bot.commands.cookieCommand = {
             command: 'cookie',
@@ -65,18 +60,18 @@
     }
 
         localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "Farofinha Bot",
+        botName: "Olá, seja bem vindo, volte sempre! :point_right: :ok_hand:",
         language: "portuguese",
-        chatLink: "https://rawgit.com/" + fork + "/basicBot/master/lang/pt.json",
-        startupCap: 1,
-        startupVolume: 0,
-        startupEmoji: false,
+        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
+        startupCap: 9,
+        startupVolume: 50,
+        startupEmoji: true,
         maximumAfk: 60,
         afkRemoval: true,
         maximumDc: 60,
-        bouncerPlus: true,
+        bouncerPlus: false,
         lockdownEnabled: false,
-        lockGuard: false,
+        lockGuard: true,
         maximumLocktime: 10,
         cycleGuard: true,
         maximumCycletime: 10,
@@ -102,22 +97,22 @@
         motdEnabled: true,
         motdInterval: 8,
         motd: "!roulette",
-        filterChat: false,
+        filterChat: true,
         etaRestriction: true,
         welcome: true,
         opLink: null,
         rulesLink: null,
         themeLink: null,
-        fbLink: "https://www.facebook.com/groups/farofadasdivaspop/",
+        fbLink: "null",
         youtubeLink: null,
-        website: "https://www.facebook.com/farofadasdivaspop",
+        website: "null",
         intervalMessages: [],
         messageInterval: 5,
         songstats: false,
         commandLiteral: "!",
         blacklists: {
-            NSFW: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-            OP: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleOPlist.json"
+            NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
         }
     }));
 
